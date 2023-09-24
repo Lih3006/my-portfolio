297e1a7cf4d1af99e9eca5d3782cd48d73cbd4cf
Lih3006 <alineb3006@hotmail.com>
2023-09-24 21:04:38 +0200

    feat:fix spaces

--- src/components/ScrollPagesComponent/index.tsx
+++ src/components/ScrollPagesComponent/index.tsx
@@ -11,8 +11,8 @@ const ScrollPagesComponent = ({ children }: { children: ReactNode }) => {
           {children}
           <p className='container--content-techs'>
             Coded in <strong>Visual Studio Code</strong>. Built with
-            <strong>React</strong> and <strong>Styled Components</strong>,
-            <strong>Axios</strong> for accessing the{' '}
+            <strong> React</strong> and <strong>Styled Components</strong>,
+            <strong> Axios</strong> for accessing the
             <strong>GitHub API </strong>
             deployed with <strong>Vercel</strong>. All text is set in the
             Lexend, and Montserrat typefaces.
