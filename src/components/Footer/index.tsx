f06626adb149f3c7eb3d03475e7e9837174e1616
Lih3006 <alineb3006@hotmail.com>
2023-09-24 21:02:37 +0200

    feat:delete console.log

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,8 +2,6 @@ import me from '../../images/me.svg';
 import { StyledFooterComponent } from './style';
 
 const Footer = () => {
-  const width = window.innerWidth;
-  console.log(width / 438);
   return (
     <StyledFooterComponent>
       <img src={me} alt='' />
